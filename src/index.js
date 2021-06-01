768ff0a946d6fbed5e4af67bf17cb19dbc5b2e14
Assim Ez-zaky <pengBerry@gmail.com>
2021-06-01 19:28:39 +0200

    get ready to start

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import reactDOM from "react-dom";
+import styled from "styled-components";
+import { App } from "./App";
+const root = document.getElementById("root");
+reactDOM.render(<App />, root);
